e0413da20ce426a6a228a7c2b78a7eff38085bb4
machearn <machearn.n@gmail.com>
2021-12-26 15:45:40 -0500

    add a executable target to client
    add `main.cpp` to client to generate compile command through cmake, so
    that clangd can find the dependency of client

---
+++ client/main.cpp
@@ -0,0 +1,15 @@
+//
+// This file is used for generate compile_command for clangd
+//
+
+#include <iostream>
+
+#include "generator.hpp"
+
+using json = nlohmann::json;
+
+int main(int argc, char *argv[]) {
+    json j;
+    std::cout << j << std::endl;
+    return 0;
+}

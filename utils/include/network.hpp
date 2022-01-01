ee217420e4287137109e4396695057bd76a9202c
machearn <machearn.n@gmail.com>
2022-01-01 16:28:21 -0500

    use RAII manage socket resources

--- utils/include/network.hpp
+++ utils/include/network.hpp
@@ -12,24 +12,24 @@
 
 namespace mrpc {
     constexpr const size_t RECV_BUF_SIZE = 4096;
-    void transit(const std::string& data, const std::string& ip, uint32_t port) {
-        std::unique_ptr<::sockaddr_in> server_socket(new ::sockaddr_in());
-        ::inet_pton(AF_INET, ip.c_str(), &(server_socket->sin_addr));
-        server_socket->sin_family = AF_INET;
-        server_socket->sin_port = htonl(port);
+    void client(const std::string& data, const std::string& ip, uint32_t port) {
+        auto remote_socket = std::make_unique<::sockaddr_in>();
+        ::inet_pton(AF_INET, ip.c_str(), &(remote_socket->sin_addr));
+        remote_socket->sin_family = AF_INET;
+        remote_socket->sin_port = htonl(port);
 
         auto socket_fp = ::socket(PF_INET, SOCK_STREAM, 0);
         assert(socket_fp);
 
-        if (::connect(socket_fp, (::sockaddr*)server_socket.get(), sizeof(*server_socket)) < 0) {
+        if (::connect(socket_fp, (::sockaddr*)remote_socket.get(), sizeof(*remote_socket)) < 0) {
             std::cout << "Failed connect to server\n";
         } else {
             ::send(socket_fp, data.c_str(), data.size(), 0);
         }
     }
 
-    int waiting(const std::string& ip, uint32_t port, std::string& data) {
-        std::unique_ptr<::sockaddr_in> host_socket(new ::sockaddr_in());
+    int server(std::string& data, const std::string& ip, uint32_t port) {
+        auto host_socket = std::make_unique<::sockaddr_in>();
         ::inet_pton(AF_INET, ip.c_str(), &(host_socket->sin_addr));
         host_socket->sin_family = AF_INET;
         host_socket->sin_port = htonl(port);
@@ -37,15 +37,15 @@ namespace mrpc {
         auto socket_fp = ::socket(PF_INET, SOCK_STREAM, 0);
         assert(socket_fp);
 
-        int ret = bind(socket_fp, (sockaddr*)host_socket.get(), sizeof(*host_socket));
+        int ret = ::bind(socket_fp, (sockaddr*)host_socket.get(), sizeof(*host_socket));
         assert(ret != -1);
-        ret = listen(socket_fp, 5);
+        ret = ::listen(socket_fp, 5);
         assert(ret != -1);
 
         std::unique_ptr<::sockaddr_in> remote_socket(new ::sockaddr_in());
-        socklen_t server_socket_len = sizeof(*remote_socket);
+        ::socklen_t remote_socket_len = sizeof(*remote_socket);
 
-        int connfd = ::accept(socket_fp, (sockaddr*)remote_socket.get(), &server_socket_len);
+        int connfd = ::accept(socket_fp, (::sockaddr*)remote_socket.get(), &remote_socket_len);
 
         if (connfd < 0) {
             std::cout << "Failed to build listening connection\n";

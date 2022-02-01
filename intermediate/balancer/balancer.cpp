2ffdbe1746b27329cd806c99bcb39788fb1cd955
machearn <machearn.n@gmail.com>
2022-02-01 16:04:53 -0500

    implement balancer

--- intermediate/balancer/balancer.cpp
+++ intermediate/balancer/balancer.cpp
@@ -1,1 +1,108 @@
 #include "include/balancer.hpp"
+
+bool mrpc::Service::isDown() const {
+    return weight == 0;
+}
+
+std::string mrpc::Service::getUrl() const {
+    return url;
+}
+
+std::int16_t mrpc::Service::getPort() const {
+    return port;
+}
+
+void mrpc::Service::updateWeight() {
+    weight--;
+}
+
+void mrpc::Service::reset() {
+    weight = INITIAL_WEIGHT;
+}
+
+std::int32_t mrpc::Service::getWeight() const {
+    return weight;
+}
+
+std::tuple<std::string, std::int16_t> mrpc::Balancer::epoll(const std::string& func_name) {
+    auto& host_list = hosts.at(func_name);
+
+    auto top = host_list.begin();
+    top->updateWeight();
+
+    auto dest = std::find_if(++host_list.cbegin(), host_list.cend(),
+                             [top](Service& s) { return s.getWeight() < top->getWeight(); });
+    host_list.splice(dest, host_list, top);
+
+    return std::make_tuple(top->getUrl(), top->getPort());
+}
+
+void mrpc::Balancer::initialize(::pid_t pid, const std::filesystem::path& fifo_path) {
+    requestData("ALL", pid, fifo_path);
+}
+
+void mrpc::Balancer::insertEntry(const std::string& func_name, std::string ip, std::int16_t port) {
+    hosts[func_name].emplace_back(std::move(ip), port, INITIAL_WEIGHT);
+    threshold[func_name] = mrpc::INITIAL_THRESHOLD;
+}
+
+void mrpc::Balancer::resetWeights(const std::string& func_name) {
+    for (auto& service: hosts[func_name]) {
+        service.reset();
+    }
+}
+
+void
+mrpc::Balancer::updateHosts(const std::string& func_name, ::pid_t pid, const fs::path& fifo_path) {
+    if (threshold[func_name] > 0) {
+        threshold[func_name]--;
+    } else {
+        requestData(func_name, pid, fifo_path);
+    }
+}
+
+void
+mrpc::Balancer::requestData(const std::string& func_name,
+                            ::pid_t pid,
+                            const std::filesystem::path& fifo_path) {
+    ::kill(pid, SIGUSR1);
+    int fifo_fd;
+
+    if ((fifo_fd = ::open(fifo_path.c_str(), O_WRONLY)) < 0) {
+        std::cerr << "Failed open FIFO" << std::endl;
+        std::cerr << "errno: " << errno << std::endl;
+    }
+    ::write(fifo_fd, func_name.c_str(), func_name.size());
+    ::close(fifo_fd);
+
+    // block until write open
+    if ((fifo_fd = ::open(fifo_path.c_str(), O_RDONLY)) < 0) {
+        std::cerr << "Failed open FIFO" << std::endl;
+        std::cerr << "errno: " << errno << std::endl;
+    }
+
+    /*
+     * json format
+     * [
+     * {
+     *      "func_name": "",
+     *      "ip": "",
+     *      "port": 0
+     * },
+     * ...
+     * ]
+     */
+    std::string data{};
+    char buf[1024];
+    ssize_t read_size;
+    while ((read_size = ::read(fifo_fd, (void*) buf, 1024)) > 0) {
+        data.append(buf);
+    }
+
+    nlohmann::json json = nlohmann::json::parse(data);
+    for (auto& element: json) {
+        insertEntry(json.at("func_name").get<std::string>(),
+                    json.at("ip").get<std::string>(),
+                    json.at("port").get<int16_t>());
+    }
+}
--- intermediate/balancer/include/balancer.hpp
+++ intermediate/balancer/include/balancer.hpp
@@ -2,52 +2,61 @@
 #define BALANCER_H
 
 #include <unistd.h>
+#include <fcntl.h>
+#include <csignal>
 
+#include <iostream>
 #include <string>
 #include <list>
 #include <unordered_map>
+#include <tuple>
+#include <filesystem>
+
+#include "nlohmann/json.hpp"
 
 namespace mrpc {
 
-const std::int32_t INITIAL_WEIGHT = 5;
+namespace fs = std::filesystem;
+const static std::int32_t INITIAL_WEIGHT = 5;
+const static std::int32_t INITIAL_THRESHOLD = 20;
+
+int balancerConfigure(const std::string&);
 
 class Service {
 private:
-    std::string serviceName;
-    std::string url;
-    std::int16_t port;
-    std::int32_t weight;
+    std::string url{};
+    std::int16_t port{};
+    std::int32_t weight{};
 
 public:
     Service() = default;
 
-    Service(const std::string& serviceName_,
-            const std::string& url_,
+    Service( std::string  url_,
             std::int16_t port_,
-            std::int32_t weight_) : serviceName(serviceName_), url(url_),
-                          port(port_), weight(weight_) {};
+            std::int32_t weight_) : url(std::move(url_)), port(port_), weight(weight_) {};
 
     ~Service() = default;
 
-    bool isDown();
+    bool isDown() const;
 
-    std::string getServiceName();
+    std::string getUrl() const;
 
-    std::string getUrl();
+    std::int16_t getPort() const;
 
-    std::int16_t getPort();
-
-    std::int32_t getWeight();
+    std::int32_t getWeight() const;
 
     void updateWeight();
 
-    void clear();
+    void reset();
 };
 
 class Balancer {
 private:
-    std::unordered_map<std::string, std::list<Service>> hosts;
-    ::pid_t registration_pid;
+    std::unordered_map<std::string, std::list<Service>> hosts{};
+    std::unordered_map<std::string, std::int32_t> threshold{};
+    ::pid_t registration_pid{};
+
+    void requestData(const std::string&, ::pid_t, const fs::path&);
 public:
     Balancer() = default;
 
@@ -56,13 +65,15 @@ public:
         return balancer;
     }
 
-    void configure(::pid_t);
+    void insertEntry(const std::string&, std::string, std::int16_t);
 
-    void epoll(const std::string&);
+    void initialize(::pid_t, const fs::path&);
 
-    bool clearWeights(const std::string&);
+    std::tuple<std::string, std::int16_t> epoll(const std::string&);
 
-    void updateHosts(const std::string&);
+    void resetWeights(const std::string&);
+
+    void updateHosts(const std::string&, ::pid_t, const fs::path&);
 };
 }// namespace mrpc
 

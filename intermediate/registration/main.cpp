d325a3ab9d2c1789f6d33917ad48ad5ac596f18a
machearn <machearn.n@gmail.com>
2022-02-14 17:52:08 -0500

    implement signal(from balancer) handle for registration process

---
+++ intermediate/registration/main.cpp
@@ -0,0 +1,51 @@
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <filesystem>
+
+#include "nlohmann/json.hpp"
+#include "include/register.hpp"
+
+std::filesystem::path fifo_path{};
+
+void sigusr1(int signo) {
+    int fifo_fd;
+    if ((fifo_fd = ::open(fifo_path.c_str(), O_RDONLY)) < 0) {
+        std::cerr << "Failed open FIFO" << std::endl;
+        std::cerr << "errno: " << errno << std::endl;
+    }
+    std::string func_name{};
+    char buf[1024];
+    while (::read(fifo_fd, buf, 1024) > 0) {
+        func_name.append(buf);
+    }
+    ::close(fifo_fd);
+
+    auto result = mrpc::Register::instance().query(func_name);
+    nlohmann::json json;
+    for (auto& element: result) {
+        json.push_back({
+                               {"func_name", func_name},
+                               {"ip", std::get<0>(element)},
+                               {"port", std::get<1>(element)}
+                       });
+    }
+    std::string json_str = json.dump();
+
+    if ((fifo_fd = ::open(fifo_path.c_str(), O_RDONLY)) < 0) {
+        std::cerr << "Failed open FIFO" << std::endl;
+        std::cerr << "errno: " << errno << std::endl;
+    }
+
+    ::write(fifo_fd, json_str.c_str(), json_str.size());
+    ::close(fifo_fd);
+}
+
+int main(int argc, char** argv) {
+    mrpc::configure(std::move(mongocxx::uri{}));
+    
+    for( ; ;) {
+        ::pause();
+    }
+    return 0;
+}

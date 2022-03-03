47c6d7cdbfa43bf68a27c9e88a3dad0a0f2b8a29
machearn <machearn.n@gmail.com>
2022-03-03 15:40:56 -0500

    delete SIGUSR2 signal handler, modify SIGUSR1 signal handler

--- intermediate/balancer/main.cpp
+++ intermediate/balancer/main.cpp
@@ -10,61 +10,14 @@ void sigusr1(int signo) {
         syslog(LOG_ERR, "open FIFO error");
         exit(errno);
     }
-    std::string command{};
-    char buf[1024];
-    while (::read(fifo_fd, buf, 1024) > 0) {
-        command.append(buf);
-    }
-    ::close(fifo_fd);
-
-    std::string op = command.substr(0, 2);
-    std::string func_name = command.substr(3);
-
-    // Normal Update
-    if (op == "NU") {
-        if (mrpc::Balancer::instance().isAvailable(func_name)) {
-            char ret = 0;
-            if ((fifo_fd = open(fifo_path.c_str(), O_WRONLY)) < 0) {
-                syslog(LOG_ERR, "open FIFO error");
-                exit(errno);
-            }
-            ::write(fifo_fd, &ret, 1);
-            ::close(fifo_fd);
-        }
-    }
-    // Force Update
-    else if (op == "FU") {
-        mrpc::Balancer::instance().requestData(func_name);
-    }
-}
-
-// handle signal from parent process
-void sigusr2(int signo) {
-    if ((fifo_fd = open(fifo_path.c_str(), O_RDONLY)) < 0) {
-        syslog(LOG_ERR, "open FIFO error");
-        exit(errno);
-    }
     std::string func_name{};
-    char buf[1024];
-    while (::read(fifo_fd, buf, 1024) > 0) {
+    char buf[1024]{};
+    while (::read(fifo_fd, buf, 1023) > 0) {
         func_name.append(buf);
     }
     ::close(fifo_fd);
 
-    std::string op = func_name.substr(0, 2);
-
-    auto ret = mrpc::Balancer::instance().epoll(func_name);
-
-    if ((fifo_fd = open(fifo_path.c_str(), O_WRONLY)) < 0) {
-        syslog(LOG_ERR, "open FIFO error");
-        exit(errno);
-    }
-    auto ip = std::get<0>(ret);
-    auto port = std::to_string(std::get<1>(ret));
-    ::write(fifo_fd, ip.c_str(), ip.size());
-    ::write(fifo_fd, ":", 1);
-    ::write(fifo_fd, port.c_str(), port.size());
-    ::close(fifo_fd);
+    mrpc::Balancer::instance().requestData(func_name);
 }
 
 int main(int argc, char** argv) {
@@ -83,11 +36,6 @@ int main(int argc, char** argv) {
     mrpc::Balancer::instance().initialize(register_pid, fifo_path);
 
     if (mrpc::signal(SIGUSR1, sigusr1) == SIG_ERR) {
-        syslog(LOG_ERR, "set signal error");
-        exit(errno);
-    }
-
-    if (mrpc::signal(SIGUSR2, sigusr2) == SIG_ERR) {
         syslog(LOG_ERR, "set signal error");
         exit(errno);
     }
